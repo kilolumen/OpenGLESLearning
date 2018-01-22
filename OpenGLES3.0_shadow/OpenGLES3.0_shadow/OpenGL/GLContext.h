9a5c5c9fd5280c84ab01dcf5998b62df336f50a1
SUNJIAN <sunjian@sensetime.com>
2018-01-23 00:30:28 +0800

    add glContext

--- OpenGLES3.0_shadow/OpenGLES3.0_shadow/OpenGL/GLContext.h
+++ OpenGLES3.0_shadow/OpenGLES3.0_shadow/OpenGL/GLContext.h
@@ -10,6 +10,28 @@
 #import <GLKit/GLKit.h>
 #import <OpenGLES/ES2/glext.h>
 
+@class GLGeometry;
 @interface GLContext : NSObject
+@property (nonatomic, assign) GLuint program;
++ (id)contextWithVertexShaderPath:(NSString *)vertexShaderPath
+               fragmentShaderPath:(NSString *)fragmentShaderPath;
+- (id)initWithVertexShader:(NSString *)vertexShader fragmentShader:(NSString *)fragmentShader;
+- (void)active;
+- (void)bindAttributes:(GLfloat *)triangleData;
 
+//draw functions
+- (void)drawTriangles:(GLfloat *)triangleData vertexCount:(GLint)vertexCount;
+- (void)drawTrianglesWithVBO:(GLuint)vbo vertexCount:(GLint)vertexCount;
+- (void)drawTrianglesWithVAO:(GLuint)vao vertexCount:(GLint)vertexCount;
+
+
+//uniform setters
+- (void)setUniform1i:(NSString *)uniformName value:(GLint)value;
+- (void)setUniform1f:(NSString *)uniformName value:(GLint)value;
+- (void)setUniform3fv:(NSString *)uniformName value:(GLKVector3)value;
+- (void)setUniform4fv:(NSString *)uniformName value:(GLKVector4)value;
+- (void)setUniformMatrix4fv:(NSString *)uniformName value:(GLKMatrix4)value;
+
+- (void)bindTexture:(GLKTextureInfo *)textureInfo to:(GLenum)textureChannel uniformName:(NSString *)uniformName;
+- (void)bindTextureName:(GLuint)textureName to:(GLenum)textureChannel uniformName:(NSString *)uniformName;
 @end
